6b9f5eee0687fd1334d4e6d6e57374e597b1d5bc
Jordan Moore <moorejm@rose-hulman.edu>
2014-03-18 19:57:17 -0400

    Starting a GUI --need to draw map on JPanel

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,28 @@
+import gui.MainPanel;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 
 public class Main {
+	private JFrame window;
 
-	/**
-	 * @param args
-	 */
+	public Main() {
+		SwingUtilities.invokeLater(new Runnable() {
+
+			@Override
+			public void run() {
+				window = new JFrame("TicketToRide Window");
+				window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				window.getContentPane().add(new MainPanel());
+				window.pack();
+				window.setVisible(true);
+			}
+		});
+
+	}
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		new Main();
 
 	}
 
