7804d6fa9d89e2d77c36d8ae77fc99ed7fd00423
Jordan Moore <moorejm@rose-hulman.edu>
2014-03-19 03:38:52 -0400

    Moved RouteTest into correct package

---
+++ src/test/GameTest.java
@@ -0,0 +1,19 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class GameTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
--- src/test/RouteTest.java
+++ src/test/RouteTest.java
@@ -1,4 +1,4 @@
-package objects;
+package test;
 
 import static org.junit.Assert.*;
 
